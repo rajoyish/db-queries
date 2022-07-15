65b1085ecfe03c10ae0945b49dc3e205f2dac159
Himalayan <hdnpress@gmail.com>
2022-07-15 17:14:30 +0545

    3.  Query events and transactions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,25 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        // This shows SQL query, binding and time on the homepage
+        DB::listen(function ($query) {
+            var_dump($query->sql);
+
+            var_dump($query->binding ?? 'no binding');
+
+            var_dump($query->time);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,30 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
 
-    // Plain SQL by PDO
-    $pdo = DB::connection()->getPdo();
-    $users = $pdo->query('select * from users')->fetchAll();
-    dump($users);
 
-    // RAW SQL
-    $result = DB::select('select * from users where id = ? && name = ?', [1, 'Dr. Roosevelt Hackett']);
-    $result = DB::select('select * from users where id = :id', ['id' => 2]);
-    DB::insert('insert into users (name, email, password) values(?, ?, ?)', ['Bobby Padilla', 'nutokebu@osiza.km', 'password']);
-    $affected = DB::update('update users set email = "rajoyish@gmail.com" where email = ?', ['nutokebu@osiza.km']);
-    dump($affected);
-    $deleted = DB::delete('delete from users where id = ?', [1]);
-    dump($deleted);
+    DB::transaction(function () {
 
+        // try catch block is not necessary as well as  DB::rollBack(). Laravel automatically rollback transation if on of transaction gets failed;
+        try {
+            DB::table('users')->delete();
 
-    // Selecting 'users' by RAW SQL
-    $rawSql = DB::select('select * from users');
-    dump($rawSql);
+            $result = DB::table('users')->where('id', 1)->update(['email' => 'none']);
+            // DB::table('users')->where('id', 3)->update(['email' => 'none@none.com']);
+
+            if (!$result) {
+                throw new \Exception;
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
+    }, 5); // optional third argument, how many times a transaction should be reattempted.
 
     // Selecting 'users' by Query Builder
     $queryBuilder = DB::table('users')->select()->get();
     dump($queryBuilder);
-
-    // Selecting 'users' by Eloquent ORM
-    $eloquentOrm = User::all();
-    dump($eloquentOrm);
-
-
-    return view('welcome');
 });
